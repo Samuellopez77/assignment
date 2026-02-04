bdd820de54b84ae52ad3d7dcf3dde7f4070c4b5a
Samuellopez77 <slasumani@st.ug.edu.gh>
2026-02-04 14:06:09 +0000

    defining methods for calculating class avarage

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,21 @@ public class Main {
         GradeManager.executeGradeReport(70);
         GradeManager.executeGradeReport(55.5);
         GradeManager.executeGradeReport(120);
-    }
+        
+        // calculating for class averages
+        System.out.println("\ncalculating class averages...");
 
+        double average1 = GradeManager.calculateClassAverage(50, 70);
+        System.out.println("Average of two:" + average1);
+
+        double average2 = GradeManager.calculateClassAverage(25, 58, 35);
+        System.out.println("Average of three:" + average2);
+
+        double[] scores = {25, 17, 30, 55, 12, 40};
+        double averageArr = GradeManager.calculateClassAverage(scores);
+        System.out.println("Average of Arr:" + averageArr);
+
+
+    }
+    
 }
